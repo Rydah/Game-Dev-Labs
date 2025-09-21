79e616e1f145470e55f295507a920cbf994d96d9
Dylan Tan <dylantanjl45@hotmail.com>
2025-09-21 23:51:07 +0800

    Changed Tiles to Prefabs

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     public Transform player; // assign Player in Inspector
     public float smoothSpeed = 0.125f; // lower = smoother
 
-    public Transform startLimit; // GameObject that indicates end of map
+    public Transform startLimit; // GameObject that indicates start of map
     public Transform endLimit; // GameObject that indicates end of map
     private float offset; // initial x-offset between camera and Mario
     private float startX; // smallest x-coordinate of the Camera
@@ -21,7 +21,7 @@ public class CameraFollow : MonoBehaviour
         Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)); // the z-component is the distance of the resulting plane from the camera 
         viewportHalfWidth = Mathf.Abs(bottomLeft.x - this.transform.position.x);
         offset = this.transform.position.x - player.position.x;
-        startX = this.transform.position.x;
+        startX = startLimit.transform.position.x + viewportHalfWidth;
         endX = endLimit.transform.position.x - viewportHalfWidth;
 
         if (player != null)
@@ -35,8 +35,11 @@ public class CameraFollow : MonoBehaviour
         if (playerMovement != null && playerMovement.isDead) return;
 
         float desiredX = player.position.x + offset;
-        // check if desiredX is within startX and endX
-        if (desiredX > startX && desiredX < endX)
-            this.transform.position = new Vector3(desiredX, this.transform.position.y, this.transform.position.z);
+        desiredX = Mathf.Clamp(desiredX, startX, endX); 
+
+        float desiredY = player.position.y;
+
+        Vector3 desiredPosition = new Vector3(desiredX, desiredY, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
     }
 }
